68b129b9595694317aa31b9042d96dca1acf2c3e
printer_scanner <abbey@fictivekin.com>
2024-03-25 12:53:25 +0100

    Update EditableContent.jsx

--- src/EditableContent.jsx
+++ src/EditableContent.jsx
@@ -16,44 +16,44 @@ function EditableContent(props) {
     setContent(getRandomSampleText());
   };
 
+  // Escape font family name to prevent CSS injection
+  const fontFamily = props.fontFamily.replace(/'/g, "\\'");
+
   return (
-    
     <div className='editableContentWrapper'>
       <div className='editableRow'>
         <div className='flex'>
-            <input
-              type="range"
-              min="16"
-              max="300"
-              value={fontSize}
-              onChange={(e) => setFontSize(e.target.value)}
-            />
-            <button onClick={handleRefreshClick} className="refresh-button">&#8635;</button>
-
-          </div>
-          {props.showDownload && <a className="refresh-button" href={`/fonts/${props.link}`}>Download ➬</a>}
-
+          <input
+            type="range"
+            min="16"
+            max="300"
+            value={fontSize}
+            onChange={(e) => setFontSize(e.target.value)}
+          />
+          <button onClick={handleRefreshClick} className="refresh-button">&#8635;</button>
         </div>
-        <style>
-          {`
-            @font-face {
-              font-family: '${props.fontFamily}';
-              src: url('https:raw.githubusercontent.com/mother-type/${props.fontFamily}/main/fonts/${props.fontFamily}-Regular.woff') format('woff');
-              /* Add additional font formats if necessary */
-            }
+        {props.showDownload && <a className="refresh-button" href={`/fonts/${props.link}`}>Download ➬</a>}
+      </div>
+      <style>
+        {`
+          @font-face {
+            font-family: '${fontFamily}';
+            src: url('https://raw.githubusercontent.com/mother-type/${props.fontFamily}/main/fonts/${props.fontFamily}-Regular.woff') format('woff');
+            /* Add additional font formats if necessary */
+          }
 
-            .${props.fontFamily}-class {
-              font-family: '${props.fontFamily}', sans-serif;
-              /* Use the custom class where you want to apply this font */
-            }
-          `}
-        </style>
+          .${props.fontFamily}-class {
+            font-family: '${fontFamily}', sans-serif;
+            /* Use the custom class where you want to apply this font */
+          }
+        `}
+      </style>
       <div
         contentEditable={true}
         spellCheck={false}
         className={props.fontFamily + "-class editableContent"}
         style={{
-          fontFamily: props.fontFamily,
+          fontFamily: fontFamily,
           fontSize: `${fontSize}px`,
         }}
         dangerouslySetInnerHTML={{ __html: content }}
