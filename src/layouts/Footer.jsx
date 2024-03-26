e5c8f4eedaa257f980ad331bbd673bd15d0d5152
printer_scanner <abbey@fictivekin.com>
2024-03-26 16:54:36 +0100

    Update Footer.jsx

--- src/layouts/Footer.jsx
+++ src/layouts/Footer.jsx
@@ -11,10 +11,10 @@ function Footer() {
             <h2 className="logo">Mother</h2>
           </a>
         </div>
-        <MenuItem href="mailto:itsmothertype@gmail.com" color="green" text="About" />
+        <MenuItem href="/about" color="green" text="About" />
         <MenuItem href="mailto:itsmothertype@gmail.com" color="red" text="Mail" />
-        <MenuItem href="mailto:itsmothertype@gmail.com" color="blue" text="Are.na" />
-        <MenuItem href="https://www.are.na/mother-type" color="gray" text="Github" />
+        <MenuItem href="https://www.are.na/mother-type" color="blue" text="Are.na" />
+        <MenuItem href="https://www.github.com/mother-type" color="gray" text="Github" />
         <div className="grid-item"></div>
       </div>
     </footer>
