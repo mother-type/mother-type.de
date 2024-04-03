a84c0003467278f75c2412cfa416690da3ca9d15
printer_scanner <abbey@fictivekin.com>
2024-04-03 13:57:44 +0200

    Add about page content

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -11,13 +11,22 @@ function About() {
   };
 
   return (
-    <div className="main About">
+    <div className="main">
       <ColorPicker onColorChange={handleColorChange} />
       <h1   
       style={{
           color: textColor,
         }}>About</h1>
       <p>Mother Type is an open source type foundry.</p>
+      <p>We are committed to making type design more accessible, and all our typefaces are available for use, distribution and collaboration via the <a href="https://openfontlicense.org">SIL Open Font License (OFL)</a>. With this license, everyone is free to use, modify, and distribute our typefaces. We ask you credit the designer and foundry when using our typefaces.</p>
+
+      <p>Our typefaces are shared and distributed via the <a href="https://github.com/mother-type/Unified-Font-Repository">Unified Font Repository</a>. These repositories also serve as the Content Management System for this website. Change or update the typefaces repository, and the site will change with it. This sites main function is as a visual directory of updatable fonts, which will provide the user with the most up-to-date version of a typeface at any time, and hopefully, avenues for collaboration.</p>
+      <h2
+        style={{
+          color: textColor,
+      }}>Submissions</h2>
+      <p>If you would like to submit a font, email us at <a href="mailto:itsmothertype@gmail.com">itsmothertype@gmail.com</a>, or make a fork of the <a href="https://github.com/mother-type/Unified-Font-Repository">Unified Font Repository</a> on Github.</p>
+
     </div>
     
   );
