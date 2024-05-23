eda8eaacfa004e622711cf0239d57a21967ebbb7
printer_scanner <abbey@fictivekin.com>
2024-05-23 15:44:22 +0200

    Update Footer.jsx

--- src/layouts/Footer.jsx
+++ src/layouts/Footer.jsx
@@ -13,7 +13,7 @@ function Footer() {
         </div>
         <MenuItem href="/about" color="green" text="About" />
         <MenuItem href="mailto:itsmothertype@gmail.com" color="red" text="Mail" />
-        <MenuItem href="https://www.are.na/mother-type" color="blue" text="Are.na" />
+        <MenuItem href="https://www.are.na/printer-scanner" color="blue" text="Are.na" />
         <MenuItem href="https://www.github.com/mother-type" color="gray" text="Github" />
         <div className="grid-item"></div>
       </div>
